f76b8bf5cfac4384ae9a9abb7dcd06a5f1bc4068
krlargo <krlargo@ucdavis.edu>
2017-12-22 15:45:23 -0800

    Remove auto submit from 'Enter'/'Submit' button pressed

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -14,9 +14,15 @@ class SearchBar extends Component {
     this.setState({ term: event.target.value });
   }
 
+  onFormSubmit(event) {
+    event.preventDefault(); // Don't auto-submit the form on 'Enter'/'Submit'
+  }
+
   render() {
     return (
-      <form className="input-group">
+      <form
+        onSubmit={this.onFormSubmit}
+        className="input-group">
         <input
           placeholder="Enter a city name"
           className="form-control"
