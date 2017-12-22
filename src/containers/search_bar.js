940eb9cc1488c268a5af2e4a14aa66e59a5bb58f
krlargo <krlargo@ucdavis.edu>
2017-12-22 15:38:33 -0800

    Add state to SearchBar

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,11 +1,24 @@
 import React, { Component} from 'react';
 
 class SearchBar extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { term: '' };
+  }
+
+  onInputChange(event) {
+    this.setState({ term: event.target.value });
+  }
 
   render() {
     return (
       <form className="input-group">
-        <input />
+        <input
+          placeholder="Enter a city name"
+          className="form-control"
+          value={this.state.term}
+          onChange={this.onInputChange.bind(this)} />
         <span className="input-group-button">
           <button type="submit" className="btn btn-secondary">Submit</button>
         </span>
