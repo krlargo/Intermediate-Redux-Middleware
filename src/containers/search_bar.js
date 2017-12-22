fbcf51a12d8cf26bf8a399e181f89b49de578438
krlargo <krlargo@ucdavis.edu>
2017-12-22 15:24:50 -0800

    Add Submit button

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -6,6 +6,9 @@ class SearchBar extends Component {
     return (
       <form className="input-group">
         <input />
+        <span className="input-group-button">
+          <button type="submit" className="btn btn-secondary">Submit</button>
+        </span>
       </form>
     )
   }
