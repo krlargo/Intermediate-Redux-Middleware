d105b79d8c88af0946f2e1070f9902cfe7d4646e
krlargo <krlargo@ucdavis.edu>
2017-12-22 18:00:07 -0800

    Fix errors

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,13 @@
 import Axios from 'axios';
 
 const API_KEY = '04ed9e4c45e3696b183cf8931a8a2e99';
-const ROOT_URL = `http://samples.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city) {
   const url = `${ROOT_URL}&q=${city},us`;
-  const request = axios.get(url); // Promise
+  const request = Axios.get(url); // Promise
 
   return {
     type: FETCH_WEATHER,
