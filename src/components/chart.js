d365ef1531087b0b8525affb2b500b388984218d
krlargo <krlargo@ucdavis.edu>
2017-12-26 23:08:44 -0800

    Add average label

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+import _ from 'lodash';
 
 const Chart = (props) => {
+
+  function average(data) {
+    return _.round(_.sum(data)/data.length);
+  }
+
   return (
     <div>
-      <Sparklines svgHeight={200} svgWidth={200} data={props.data}>
+      <Sparklines svgHeight={150} svgWidth={200} data={props.data}>
         <SparklinesLine color={props.color} />
         <SparklinesReferenceLine type="mean" />
+        <div>{average(props.data)}</div>
       </Sparklines>
     </div>
   )
